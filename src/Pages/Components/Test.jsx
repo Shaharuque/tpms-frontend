679001bada78f1befb127f5da3cbeddab3b49ac2
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2022-06-25 13:46:01 +0600

    add insurance

--- src/Pages/Components/Test.jsx
+++ src/Pages/Components/Test.jsx
@@ -1,16 +1,42 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import { FixedSizeList } from "react-window";
+
+const renderRow = (props) => {
+  const { index, style } = props;
+
+  return (
+    <ListItem style={style} key={index} component="div" disablePadding>
+      <ListItemButton>
+        <ListItemText primary={`Item ${index + 1}`} />
+      </ListItemButton>
+    </ListItem>
+  );
+};
 
 const Test = () => {
-  // const openMessageBox = (el) => {
-  //   var name = el.textContent;
-  //   // alert(name);
-  //   console.log(name);
-  // };
   return (
-    <ul>
-      {/* <li onclick={openMessageBox(this)}>Alice</li>
-      <li onclick={openMessageBox(this)}>Bob</li> */}
-    </ul>
+    <Box
+      sx={{
+        width: "100%",
+        height: 400,
+        maxWidth: 360,
+        bgcolor: "background.paper",
+      }}
+    >
+      <FixedSizeList
+        height={400}
+        width={360}
+        itemSize={46}
+        itemCount={200}
+        overscanCount={5}
+      >
+        {renderRow}
+      </FixedSizeList>
+    </Box>
   );
 };
 
