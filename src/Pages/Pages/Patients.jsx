8df07791047f11ec9abbdc4a8afe79e0e589a351
unknown <shahrukhamin27@gmail.com>
2022-08-11 19:24:37 +0600

    patients table done

--- src/Pages/Pages/Patients.jsx
+++ src/Pages/Pages/Patients.jsx
@@ -3,27 +3,60 @@ import { useFilters, usePagination, useSortBy, useTable } from "react-table";
 import { PatientsColumnsColumn } from "./Patients/PatientsColumns";
 import axios from "axios";
 import { memo } from "react";
-import {headers} from '../../Misc/BaseClient'
+import { headers } from '../../Misc/BaseClient'
 import Loading from "../../Loading/Loading";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Patients = () => {
   // root Patient component
-  const [tableData, setTableData] = useState([]);
+  const [items, setItems] = useState([]);
+  const [hasMore, sethasMore] = useState(true);
+  const [page, setpage] = useState(2);
 
   // get data from API
   useEffect(() => {
-    axios({
-      method: "get",
-      url: `https://ovh.therapypms.com/api/v1/admin/ac/patient`,
-      headers: headers,
-    }).then(function (res) {
-      // console.log("data =", res.data?.clients?.data);
-      setTableData(res.data?.clients?.data);
-    });
-  }, []);
-  
+    const getComments = async () => {
+      const res = await axios({
+        method: "get",
+        url: `https://ovh.therapypms.com/api/v1/admin/ac/patient?page=1`,
+        headers: headers,
+      });
+      // const result = await res.json();
+      const data = res.data?.clients?.data;
+      //console.log(data)
+      setItems(data);
+    };
 
-  const data = useMemo(() => tableData, [tableData]);
+    getComments();
+  }, []);
+
+  const fetchComments = async () => {
+    const res = await axios({
+      method: "get",
+      url: `https://ovh.therapypms.com/api/v1/admin/ac/patient?page=${page}`,
+      headers: headers,
+    });
+    const data = res.data?.clients?.data;
+    //console.log(data)
+    return data;
+  };
+
+  const fetchData = async () => {
+    const commentsFormServer = await fetchComments();
+    console.log(commentsFormServer)
+
+    setItems([...items, ...commentsFormServer]);
+
+    if (commentsFormServer.length === 0) {
+      sethasMore(false);
+    }
+    setpage(page + 1);
+  };
+  //console.log(items)
+  console.log(page)
+
+
+  const data = useMemo(() => items, [items]);
   const columns = useMemo(() => [...PatientsColumnsColumn], []);
 
   const {
@@ -42,65 +75,72 @@ const Patients = () => {
   }
 
   return (
-    <div className="h-[100vh]">
+    <div>
       <h1 className="text-lg mb-2 text-orange-400">All Patients</h1>
       {
         <div>
           <div className="pb-3 overflow-y-hidden">
-            <table
-              className="border overflow-scroll  sm:w-full "
-              {...getTableProps()}
+            <InfiniteScroll
+              dataLength={rows.length}
+              next={fetchData}
+              hasMore={hasMore}
+              loader={<h1 className="text-teal-800 font-bold text-center">Loading...</h1>}
             >
-              <thead>
-                {headerGroups.map((headerGroup) => (
-                  <tr {...headerGroup.getHeaderGroupProps()}>
-                    {headerGroup.headers.map((column) => (
-                      <th
-                        className="bg-secondary border px-1 min-w-[120px]  py-1 text-xs font-normal text-white"
-                        {...column.getHeaderProps(
-                          column.getSortByToggleProps()
-                        )}
-                      >
-                        {column.render("Header")}
+              <table
+                className="border overflow-scroll  sm:w-full "
+                {...getTableProps()}
+              >
+                <thead>
+                  {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                      {headerGroup.headers.map((column) => (
+                        <th
+                          className="bg-secondary border px-1 min-w-[120px]  py-1 text-xs font-normal text-white"
+                          {...column.getHeaderProps(
+                            column.getSortByToggleProps()
+                          )}
+                        >
+                          {column.render("Header")}
 
-                        <span className="">
-                          {column.isSorted
-                            ? column.isSortedDesc
-                              ? " ⇓ "
-                              : " ⇑ "
-                            : ""}
-                        </span>
-                        <div>
-                          {column.canFilter ? column.render("Filter") : null}
-                        </div>
-                      </th>
-                    ))}
-                  </tr>
-                ))}
-              </thead>
-              <tbody {...getTableBodyProps()}>
-                {rows.map((row) => {
-                  prepareRow(row);
-                  return (
-                    <tr {...row.getRowProps()}>
-                      {row.cells.map((cell) => {
-                        return (
-                          <td
-                            {...cell.getCellProps()}
-                            style={{
-                              border: "solid 1px #aeaeae55",
-                            }}
-                            className="text-xs py-[3px] w-10 md:w-24 text-center text-gray-600 "
-                          >
-                            {cell.render("Cell")}
-                          </td>
-                        );
-                      })}
+                          <span className="">
+                            {column.isSorted
+                              ? column.isSortedDesc
+                                ? " ⇓ "
+                                : " ⇑ "
+                              : ""}
+                          </span>
+                          <div>
+                            {column.canFilter ? column.render("Filter") : null}
+                          </div>
+                        </th>
+                      ))}
                     </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+                  ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                  {rows.map((row) => {
+                    prepareRow(row);
+                    return (
+                      <tr {...row.getRowProps()}>
+                        {row.cells.map((cell) => {
+                          return (
+                            <td
+                              {...cell.getCellProps()}
+                              style={{
+                                border: "solid 1px #aeaeae55",
+                              }}
+                              className="text-xs py-[3px] w-10 md:w-24 text-center text-gray-600 "
+                            >
+                              {cell.render("Cell")}
+                            </td>
+                          );
+                        })}
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </InfiniteScroll>
           </div>
         </div>
       }
