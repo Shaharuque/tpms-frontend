9c80cdc900567b52a5859fedec30ac6c41f8fef5
unknown <shahrukhamin27@gmail.com>
2022-08-12 12:00:42 +0600

    NameLocation date gathered

--- src/Pages/Pages/Settings/NameLocation.jsx
+++ src/Pages/Pages/Settings/NameLocation.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NameLocationTable from "./NameLocation/NameLocationTable";
 import NameLocationTable32 from "./NameLocation/NameLocationTable32";
+//Redux
+import { useDispatch, useSelector } from "react-redux";
+import { getsettings } from "../../../features/Settings_redux/settingSlice";
+import Loading from "../../../Loading/Loading";
 
 const NameLocation = () => {
   // Parent
+   //Redux works will be done here
+   const dispatch = useDispatch();
+  
+   //response from async action
+   const data = useSelector((state) => state.settingInfo); //After action dispatched response can be received here
+   
+   //Some Important data showing below
+   const loading=data?.loading;
+   const settingDetails=data?.settingDetails;
+   const box_no_32=settingDetails?.box_no_32;
+   const box_no_33=settingDetails?.box_no_33;
+   const pos=settingDetails?.pos;
+   const working_hours=settingDetails?.working_hours;
+   console.log(working_hours)
+   
+   //getsettings action is dispatched [api calling]
+   useEffect(() => {
+     dispatch(getsettings());
+   }, []);
+ 
+   if(loading){
+     return <Loading></Loading>
+   }
+ 
   return (
     <div className="p-2 ">
       <h1 className=" text-orange-500">Facility Setup</h1>
