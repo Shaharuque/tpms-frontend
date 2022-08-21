87d7410607989756b3536558de89da51ae71681d
Md Jakir Uddin Jahid <55592996+jahids@users.noreply.github.com>
2022-08-21 15:10:04 +0600

    animation bug-fixed, graph api call

--- src/Pages/Components/Sidebar.jsx
+++ src/Pages/Components/Sidebar.jsx
@@ -193,7 +193,6 @@ const initialDropState = {};
 // const x = menuItem.map((item) => {
 //   if (item.subRoute) initialDropState[item.name] = false;
 // });
-
 // console.log(x);
 // console.log(initialDropState);
 
@@ -242,9 +241,10 @@ const Sidebar = ({ handle }) => {
         <div
           style={{
             width: isHovering ? "220px" : "70px",
-            transition: isHovering ? "ease-in 0.3s" : "ease-out 0.5s",
+            // transition: isHovering ? "ease-in 0.5s" : "ease-out 0.3s",
           }}
           className="sidebar"
+          
         >
           <div className="top-section">
             {isHovering ? (
@@ -266,7 +266,7 @@ const Sidebar = ({ handle }) => {
                   <NavLink
                     to={"#"}
                     key={index}
-                    className=""
+      
                     activeclassname="active"
                   >
                     <SidebarMenu
@@ -289,12 +289,14 @@ const Sidebar = ({ handle }) => {
 
                       <div
                         // onClick={() => setOpen(!open)}
-                        style={{
-                          display: isHovering ? "block" : "none",
-                        }}
-                        className="link_text text-sm"
+                        // style={{
+                        //   display: isHovering ? "block" : "none",
+                        // }}
+                        // className="link_text text-sm"
+                        className={ isHovering ? "opacity-1 duration-600 ease-in" : "opacity-0 duration-200 ease-out"}
                       >
                         {items.name}
+                      
                       </div>
                     </div>
                   </NavLink>
