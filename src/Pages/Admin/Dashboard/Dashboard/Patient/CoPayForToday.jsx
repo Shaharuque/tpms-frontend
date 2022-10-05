dcb37a562d5f4bf8fb98cb2ac9f6f5c065c66e07
yakubsiyam <itsyakubsiyam@gmail.com>
2022-10-05 16:18:28 +0600

    (feat): Adding date range in Copay For Today table

--- src/Pages/Admin/Dashboard/Dashboard/Patient/CoPayForToday.jsx
+++ src/Pages/Admin/Dashboard/Dashboard/Patient/CoPayForToday.jsx
@@ -6,9 +6,12 @@ import { IoCaretBackCircleOutline } from "react-icons/io5";
 import { FiDownload } from "react-icons/fi";
 import axios from "axios";
 import UseTable from "../../../../../Utilities/UseTable";
+import { Table } from "antd";
 
 const CoPayForToday = () => {
   const [CoPayData, SetCoPayData] = useState([]);
+  const [filteredInfo, setFilteredInfo] = useState({});
+  const [sortedInfo, setSortedInfo] = useState({});
 
   // fakedb call
   useEffect(() => {
@@ -21,10 +24,306 @@ const CoPayForToday = () => {
       });
   }, []);
 
-  const data = useMemo(() => CoPayData, [CoPayData]);
-  const columns = useMemo(() => [...CoPayForTodayColumn], []);
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ columns, data }, useSortBy, usePagination, useRowSelect);
+  console.log(CoPayData);
+
+  // ---------------------------------Table Data-------------------------
+  const handleChange = (pagination, filters, sorter) => {
+    console.log("Various parameters", pagination, filters, sorter);
+    setFilteredInfo(filters);
+    setSortedInfo(sorter);
+  };
+
+  const columns = [
+    {
+      title: "Region",
+      dataIndex: "region",
+      key: "region",
+      width: 100,
+      // filters: [
+      //   { text: "Celestine", value: "Celestine" },
+      //   { text: "Annaliese", value: "Annaliese" },
+      //   {
+      //     text: `Maude`,
+      //     value: "Maude",
+      //   },
+      //   {
+      //     text: `Molly`,
+      //     value: "Molly",
+      //   },
+      //   {
+      //     text: "Karla",
+      //     value: "Karla",
+      //   },
+      //   {
+      //     text: "Marcellus",
+      //     value: "Marcellus",
+      //   },
+      //   {
+      //     text: "Hilton",
+      //     value: "Hilton",
+      //   },
+      // ],
+      filteredValue: filteredInfo.region || null,
+      onFilter: (value, record) => record.region.includes(value),
+      sorter: (a, b) => {
+        return a.region > b.region ? -1 : 1;
+      },
+      sortOrder: sortedInfo.columnKey === "region" ? sortedInfo.order : null,
+
+      // render contains what we want to reflect as our data
+      // region, id, key=>each row data(object) property value can be accessed.
+      render: (_, { region, id, key }) => {
+        //console.log("tags : ", client_first_name, id, key);
+        return (
+          <div>
+            <button>{region}</button>
+          </div>
+        );
+      },
+      ellipsis: true,
+    },
+    {
+      title: "Last Name",
+      dataIndex: "patient",
+      key: "patient",
+      width: 100,
+      // filters: [
+      //   { text: "Celestine", value: "Celestine" },
+      //   { text: "Annaliese", value: "Annaliese" },
+      //   {
+      //     text: `Maude`,
+      //     value: "Maude",
+      //   },
+      //   {
+      //     text: `Molly`,
+      //     value: "Molly",
+      //   },
+      //   {
+      //     text: "Karla",
+      //     value: "Karla",
+      //   },
+      //   {
+      //     text: "Marcellus",
+      //     value: "Marcellus",
+      //   },
+      //   {
+      //     text: "Hilton",
+      //     value: "Hilton",
+      //   },
+      // ],
+      filteredValue: filteredInfo.patient || null,
+      onFilter: (value, record) => record.patient.includes(value),
+      sorter: (a, b) => {
+        return a.patient > b.patient ? -1 : 1;
+      },
+      sortOrder: sortedInfo.columnKey === "patient" ? sortedInfo.order : null,
+
+      // render contains what we want to reflect as our data
+      // patient, id, key=>each row data(object) property value can be accessed.
+      render: (_, { patient, id, key }) => {
+        //console.log("tags : ", client_first_name, id, key);
+        return (
+          <div>
+            <button>{patient}</button>
+          </div>
+        );
+      },
+      ellipsis: true,
+    },
+    {
+      title: "First Name",
+      dataIndex: "provider",
+      key: "provider",
+      width: 100,
+      // filters: [
+      //   { text: "Celestine", value: "Celestine" },
+      //   { text: "Annaliese", value: "Annaliese" },
+      //   {
+      //     text: `Maude`,
+      //     value: "Maude",
+      //   },
+      //   {
+      //     text: `Molly`,
+      //     value: "Molly",
+      //   },
+      //   {
+      //     text: "Karla",
+      //     value: "Karla",
+      //   },
+      //   {
+      //     text: "Marcellus",
+      //     value: "Marcellus",
+      //   },
+      //   {
+      //     text: "Hilton",
+      //     value: "Hilton",
+      //   },
+      // ],
+      filteredValue: filteredInfo.provider || null,
+      onFilter: (value, record) => record.provider.includes(value),
+      sorter: (a, b) => {
+        return a.provider > b.provider ? -1 : 1;
+      },
+      sortOrder: sortedInfo.columnKey === "provider" ? sortedInfo.order : null,
+
+      // render contains what we want to reflect as our data
+      // patient, id, key=>each row data(object) property value can be accessed.
+      render: (_, { provider, id, key }) => {
+        //console.log("tags : ", client_first_name, id, key);
+        return (
+          <div>
+            <button>{provider}</button>
+          </div>
+        );
+      },
+      ellipsis: true,
+    },
+    {
+      title: "Supervisor",
+      dataIndex: "supervisor",
+      key: "supervisor",
+      width: 100,
+      // filters: [
+      //   { text: "Celestine", value: "Celestine" },
+      //   { text: "Annaliese", value: "Annaliese" },
+      //   {
+      //     text: `Maude`,
+      //     value: "Maude",
+      //   },
+      //   {
+      //     text: `Molly`,
+      //     value: "Molly",
+      //   },
+      //   {
+      //     text: "Karla",
+      //     value: "Karla",
+      //   },
+      //   {
+      //     text: "Marcellus",
+      //     value: "Marcellus",
+      //   },
+      //   {
+      //     text: "Hilton",
+      //     value: "Hilton",
+      //   },
+      // ],
+      filteredValue: filteredInfo.supervisor || null,
+      onFilter: (value, record) => record.supervisor.includes(value),
+      sorter: (a, b) => {
+        return a.supervisor > b.supervisor ? -1 : 1;
+      },
+      sortOrder:
+        sortedInfo.columnKey === "supervisor" ? sortedInfo.order : null,
+
+      // render contains what we want to reflect as our data
+      // supervisor, id, key=>each row data(object) property value can be accessed.
+      render: (_, { supervisor, id, key }) => {
+        //console.log("tags : ", client_first_name, id, key);
+        return (
+          <div>
+            <button>{supervisor}</button>
+          </div>
+        );
+      },
+      ellipsis: true,
+    },
+    {
+      title: "Date",
+      dataIndex: "date",
+      key: "date",
+      width: 100,
+      // filters: [
+      //   { text: "Celestine", value: "Celestine" },
+      //   { text: "Annaliese", value: "Annaliese" },
+      //   {
+      //     text: `Maude`,
+      //     value: "Maude",
+      //   },
+      //   {
+      //     text: `Molly`,
+      //     value: "Molly",
+      //   },
+      //   {
+      //     text: "Karla",
+      //     value: "Karla",
+      //   },
+      //   {
+      //     text: "Marcellus",
+      //     value: "Marcellus",
+      //   },
+      //   {
+      //     text: "Hilton",
+      //     value: "Hilton",
+      //   },
+      // ],
+      filteredValue: filteredInfo.date || null,
+      onFilter: (value, record) => record.date.includes(value),
+      sorter: (a, b) => {
+        return a.date > b.date ? -1 : 1;
+      },
+      sortOrder: sortedInfo.columnKey === "date" ? sortedInfo.order : null,
+
+      // render contains what we want to reflect as our data
+      // date, id, key=>each row data(object) property value can be accessed.
+      render: (_, { date, id, key }) => {
+        //console.log("tags : ", client_first_name, id, key);
+        return (
+          <div>
+            <button>{date}</button>
+          </div>
+        );
+      },
+      ellipsis: true,
+    },
+    {
+      title: "Copay",
+      dataIndex: "copay",
+      key: "copay",
+      width: 100,
+      // filters: [
+      //   { text: "Celestine", value: "Celestine" },
+      //   { text: "Annaliese", value: "Annaliese" },
+      //   {
+      //     text: `Maude`,
+      //     value: "Maude",
+      //   },
+      //   {
+      //     text: `Molly`,
+      //     value: "Molly",
+      //   },
+      //   {
+      //     text: "Karla",
+      //     value: "Karla",
+      //   },
+      //   {
+      //     text: "Marcellus",
+      //     value: "Marcellus",
+      //   },
+      //   {
+      //     text: "Hilton",
+      //     value: "Hilton",
+      //   },
+      // ],
+      filteredValue: filteredInfo.copay || null,
+      onFilter: (value, record) => record.copay.includes(value),
+      sorter: (a, b) => {
+        return a.copay > b.copay ? -1 : 1;
+      },
+      sortOrder: sortedInfo.columnKey === "copay" ? sortedInfo.order : null,
+
+      // render contains what we want to reflect as our data
+      // copay, id, key=>each row data(object) property value can be accessed.
+      render: (_, { copay, id, key }) => {
+        //console.log("tags : ", client_first_name, id, key);
+        return (
+          <div>
+            <button>{copay}</button>
+          </div>
+        );
+      },
+      ellipsis: true,
+    },
+  ];
   return (
     <div className={!CoPayData ? "h-[100vh]" : ""}>
       <div className="flex items-center flex-wrap gap-2 justify-between">
@@ -40,13 +339,18 @@ const CoPayForToday = () => {
         </div>
       </div>
       <div className="my-2">
-        <UseTable
-          getTableProps={getTableProps}
-          headerGroups={headerGroups}
-          getTableBodyProps={getTableBodyProps}
-          rows={rows}
-          prepareRow={prepareRow}
-        ></UseTable>
+        <div className=" overflow-scroll">
+          <Table
+            rowKey="id" //warning issue solve ar jnno unique id rowKey hisabey use hobey
+            pagination={false} //pagination dekhatey chailey just 'true' korey dilei hobey
+            size="small"
+            className=" text-xs font-normal"
+            columns={columns}
+            dataSource={CoPayData} //Which data chunk you want to show in table
+            // For fixed header table at top
+            onChange={handleChange}
+          />
+        </div>
       </div>
     </div>
   );
