f599310cd38aeba95fa1b4cfdf6f598fbb627236
yakubsiyam <itsyakubsiyam@gmail.com>
2022-10-05 15:46:09 +0600

    (feat): Adding Ant design in SelfPayClients table

--- src/Pages/Admin/Dashboard/Dashboard/Patient/SelfPayClients.jsx
+++ src/Pages/Admin/Dashboard/Dashboard/Patient/SelfPayClients.jsx
@@ -6,9 +6,12 @@ import { IoCaretBackCircleOutline } from "react-icons/io5";
 import { FiDownload } from "react-icons/fi";
 import axios from "axios";
 import UseTable from "../../../../../Utilities/UseTable";
+import { Table } from "antd";
 
 const SelfPayClients = () => {
   const [SelfPayData, SetSelfPayData] = useState([]);
+  const [filteredInfo, setFilteredInfo] = useState({});
+  const [sortedInfo, setSortedInfo] = useState({});
 
   // fake Api call
 
@@ -24,14 +27,165 @@ const SelfPayClients = () => {
 
   console.log(SelfPayData);
 
-  const data = useMemo(() => SelfPayData, [SelfPayData]);
-  const columns = useMemo(() => [...SelfPayClientsColumn], []);
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ columns, data }, useSortBy, usePagination, useRowSelect);
+  // ---------------------------------Table Data-------------------------
+  const handleChange = (pagination, filters, sorter) => {
+    console.log("Various parameters", pagination, filters, sorter);
+    setFilteredInfo(filters);
+    setSortedInfo(sorter);
+  };
+
+  const columns = [
+    {
+      title: "Patient Last Name",
+      dataIndex: "patient",
+      key: "patient",
+      width: 100,
+      // filters: [
+      //   { text: "Celestine", value: "Celestine" },
+      //   { text: "Annaliese", value: "Annaliese" },
+      //   {
+      //     text: `Maude`,
+      //     value: "Maude",
+      //   },
+      //   {
+      //     text: `Molly`,
+      //     value: "Molly",
+      //   },
+      //   {
+      //     text: "Karla",
+      //     value: "Karla",
+      //   },
+      //   {
+      //     text: "Marcellus",
+      //     value: "Marcellus",
+      //   },
+      //   {
+      //     text: "Hilton",
+      //     value: "Hilton",
+      //   },
+      // ],
+      filteredValue: filteredInfo.patient || null,
+      onFilter: (value, record) => record.patient.includes(value),
+      sorter: (a, b) => {
+        return a.patient > b.patient ? -1 : 1;
+      },
+      sortOrder: sortedInfo.columnKey === "patient" ? sortedInfo.order : null,
+
+      // render contains what we want to reflect as our data
+      // patient, id, key=>each row data(object) property value can be accessed.
+      render: (_, { patient, id, key }) => {
+        //console.log("tags : ", client_first_name, id, key);
+        return (
+          <div>
+            <button>{patient}</button>
+          </div>
+        );
+      },
+      ellipsis: true,
+    },
+    {
+      title: "Patient First Name",
+      dataIndex: "provider",
+      key: "provider",
+      width: 100,
+      // filters: [
+      //   { text: "Celestine", value: "Celestine" },
+      //   { text: "Annaliese", value: "Annaliese" },
+      //   {
+      //     text: `Maude`,
+      //     value: "Maude",
+      //   },
+      //   {
+      //     text: `Molly`,
+      //     value: "Molly",
+      //   },
+      //   {
+      //     text: "Karla",
+      //     value: "Karla",
+      //   },
+      //   {
+      //     text: "Marcellus",
+      //     value: "Marcellus",
+      //   },
+      //   {
+      //     text: "Hilton",
+      //     value: "Hilton",
+      //   },
+      // ],
+      filteredValue: filteredInfo.provider || null,
+      onFilter: (value, record) => record.provider.includes(value),
+      sorter: (a, b) => {
+        return a.provider > b.provider ? -1 : 1;
+      },
+      sortOrder: sortedInfo.columnKey === "provider" ? sortedInfo.order : null,
+
+      // render contains what we want to reflect as our data
+      // patient, id, key=>each row data(object) property value can be accessed.
+      render: (_, { provider, id, key }) => {
+        //console.log("tags : ", client_first_name, id, key);
+        return (
+          <div>
+            <button>{provider}</button>
+          </div>
+        );
+      },
+      ellipsis: true,
+    },
+    {
+      title: "Supervisor",
+      dataIndex: "Dos",
+      key: "Dos",
+      width: 100,
+      // filters: [
+      //   { text: "Celestine", value: "Celestine" },
+      //   { text: "Annaliese", value: "Annaliese" },
+      //   {
+      //     text: `Maude`,
+      //     value: "Maude",
+      //   },
+      //   {
+      //     text: `Molly`,
+      //     value: "Molly",
+      //   },
+      //   {
+      //     text: "Karla",
+      //     value: "Karla",
+      //   },
+      //   {
+      //     text: "Marcellus",
+      //     value: "Marcellus",
+      //   },
+      //   {
+      //     text: "Hilton",
+      //     value: "Hilton",
+      //   },
+      // ],
+      filteredValue: filteredInfo.Dos || null,
+      onFilter: (value, record) => record.Dos.includes(value),
+      sorter: (a, b) => {
+        return a.Dos > b.Dos ? -1 : 1;
+      },
+      sortOrder: sortedInfo.columnKey === "Dos" ? sortedInfo.order : null,
+
+      // render contains what we want to reflect as our data
+      // Dos, id, key=>each row data(object) property value can be accessed.
+      render: (_, { Dos, id, key }) => {
+        //console.log("tags : ", client_first_name, id, key);
+        return (
+          <div>
+            <button>{Dos}</button>
+          </div>
+        );
+      },
+      ellipsis: true,
+    },
+  ];
   return (
     <div className={!SelfPayData ? "h-[100vh]" : ""}>
       <div className="flex items-center flex-wrap gap-2 justify-between">
-        <h1 className="text-lg my-2 text-orange-500">Self-Pay Clients</h1>
+        <h1 className="text-lg my-2 text-orange-500">
+          Patient/Guarantor pay Clients
+        </h1>
         <div className="flex items-center gap-3">
           <FiDownload className="text-secondary font-medium" />
           <Link
@@ -44,13 +198,18 @@ const SelfPayClients = () => {
         </div>
       </div>
       <div className="my-2">
-        <UseTable
-          getTableProps={getTableProps}
-          headerGroups={headerGroups}
-          getTableBodyProps={getTableBodyProps}
-          rows={rows}
-          prepareRow={prepareRow}
-        ></UseTable>
+        <div className=" overflow-scroll">
+          <Table
+            rowKey="id" //warning issue solve ar jnno unique id rowKey hisabey use hobey
+            pagination={false} //pagination dekhatey chailey just 'true' korey dilei hobey
+            size="small"
+            className=" text-xs font-normal"
+            columns={columns}
+            dataSource={SelfPayData} //Which data chunk you want to show in table
+            // For fixed header table at top
+            onChange={handleChange}
+          />
+        </div>
       </div>
     </div>
   );
