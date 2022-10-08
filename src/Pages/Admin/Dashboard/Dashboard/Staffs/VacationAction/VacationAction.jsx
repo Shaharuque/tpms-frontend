aecc551b531cc91dc5a43f3c5b87934b9bf5afd7
yakubsiyam <itsyakubsiyam@gmail.com>
2022-10-08 15:35:17 +0600

    (feat): Adding clear filter button in Vacation Pending Approval Table

--- src/Pages/Admin/Dashboard/Dashboard/Staffs/VacationAction/VacationAction.jsx
+++ src/Pages/Admin/Dashboard/Dashboard/Staffs/VacationAction/VacationAction.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { BsThreeDots } from "react-icons/bs";
+import { BsXCircle } from "react-icons/bs";
+import { FiCheckCircle } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
 const VacationAction = () => {
@@ -8,22 +9,25 @@ const VacationAction = () => {
     setOpen(!open);
   };
   return (
-    <div>
-      <BsThreeDots
-        onClick={handleOpenAction}
-        className="text-secondary mx-auto text-sm"
-      />
-      {open && (
-        <div className="bg-white absolute right-12 mt-2 px-5 py-3">
-          <Link className="text-xs hover:text-secondary" to={"/admin"}>
-            Approve
-          </Link>
-          <br />
-          <Link className="text-xs hover:text-secondary" to={"/admin"}>
-            Reject
-          </Link>
-        </div>
-      )}
+    <div className="bg-white py-2 border shadow-xl flex flex-col items-center z-30  w-[130px] rounded-sm">
+      <div>
+        <Link
+          className="text-xs text-secondary px-2 py-1 mb-[-6px] rounded-sm hover:text-white hover:bg-secondary flex items-center font-bold gap-1 w-[110px] border border-secondary"
+          to={"/admin"}
+        >
+          <FiCheckCircle className="text-sm" />
+          Approve
+        </Link>
+        <br />
+        <Link
+          className="text-xs text-secondary px-2 py-1
+           rounded-sm hover:text-white hover:bg-secondary flex items-center font-bold gap-1 w-[110px] border border-secondary"
+          to={"/admin"}
+        >
+          <BsXCircle className="text-sm" />
+          Reject
+        </Link>
+      </div>
     </div>
   );
 };
