fe9604d808d3cc3662c2ed674f3706272f48163b
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2022-10-15 19:10:14 +0600

    random responsive changes

--- src/Pages/Admin/Staff/AddStaff/CreateStaff.jsx
+++ src/Pages/Admin/Staff/AddStaff/CreateStaff.jsx
@@ -10,7 +10,7 @@ const CreateStaff = () => {
     reset();
   };
   return (
-    <div className="h-[100vh]">
+    <div className="sm:h-[100vh]">
       <div className="flex items-center flex-wrap gap-2 justify-between">
         <h1 className="text-lg my-2 text-orange-500">Today's Copay</h1>
         <div className="flex items-center gap-3">
