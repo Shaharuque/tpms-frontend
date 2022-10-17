3e9c642ec8edb839c0216da75401b1f80c33c356
jahids <evanjahid321@gmail.com>
2022-10-17 01:25:40 -0700

    updated manageclaim actions and modal

---
+++ src/Pages/Admin/Billing/BillingManager/ManageClaims/ManageClaimAction/ManageClaimsTableAction.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import { AiFillEye, AiOutlineEye, AiOutlinePlus } from "react-icons/ai";
+import { MdHistory, MdOutlineEditNote, MdOutlineModeEditOutline } from "react-icons/md";
+import { Menu } from "antd";
+import SessionAddNote from "../../../../Appointment/ListView/ListView/SessionAddNote";
+import SessionViewNote from "../../../../Appointment/ListView/ListView/SessionViewNote";
+import EditSession from "../../../../Appointment/ListView/ListView/EditSession";
+import { useOutsideAlerter } from "../../../../../../CustomHooks/useDetectOutsideClick";
+import CorrectedClaim from "./CorrectedClaim";
+import { RiDownloadLine } from "react-icons/ri";
+
+const ManageClaimsTableAction = ({ row }) => {
+  const { ref, visible, setVisible } = useOutsideAlerter(false);
+  const [openAddNote, setOpenAddNote] = useState(false);
+  const [openViewNote, setOpenViewNote] = useState(false);
+  const [editSession, setEditSession] = useState(false);
+  //const [open, setOpen] = useState(false);
+
+  const handleOpenAction = (e) => {
+    e.preventDefault();
+    setVisible(!visible);
+  };
+
+  const addNoteHandler = () => {
+    setVisible(false);
+    setOpenAddNote(true);
+  };
+  const viewNoteHandler = () => {
+    setVisible(false);
+    setOpenViewNote(true);
+  };
+  const editSessionHandler = () => {
+    setVisible(false);
+    setEditSession(true);
+  };
+
+  const handleClose = () => {
+    setOpenAddNote(false);
+    setOpenViewNote(false);
+    setEditSession(false);
+  };
+
+  return (
+    <div>
+      {!editSession && !openAddNote && !openViewNote ? (
+        <div className=" bg-white py-2 border shadow-xl flex flex-col items-center z-30  w-[130px] rounded-sm">
+          <div>
+            <button
+              className="text-xs text-secondary px-2 py-1 mb-2 rounded-sm hover:text-white hover:bg-secondary flex items-center font-bold gap-1 w-[110px] border border-secondary"
+              onClick={() => addNoteHandler()}
+            >
+              <AiFillEye className="text-sm" /> View Hcfa
+            </button>
+            <button
+              className="text-xs text-secondary px-2 py-1 mb-2 rounded-sm hover:text-white hover:bg-secondary flex items-center font-bold gap-1 w-[110px] border border-secondary"
+              onClick={viewNoteHandler}
+            >
+              <RiDownloadLine className="text-sm" /> Downlod Edi
+            </button>
+            <button
+              className="text-xs text-secondary px-2 py-1 rounded-sm hover:text-white hover:bg-secondary flex items-center font-bold gap-1 w-[110px] border border-secondary"
+              onClick={editSessionHandler}
+            >
+              <MdHistory className="text-sm" /> View History
+            </button>
+
+            <button
+              className="text-xs text-secondary mt-2 py-1 rounded-sm hover:text-white hover:bg-secondary flex items-center font-bold  w-[110px] border border-secondary"
+              onClick={editSessionHandler}
+            >
+              <MdOutlineEditNote className="text-sm" /> Corrected Claim
+            </button>
+          </div>
+        </div>
+      ) : null}
+      {openAddNote && (
+        <SessionAddNote
+          handleClose={handleClose}
+          open={openAddNote}
+          editableRow={row}
+        ></SessionAddNote>
+      )}
+      {openViewNote && (
+        <SessionViewNote
+          handleClose={handleClose}
+          open={openViewNote}
+          editableRow={row}
+        ></SessionViewNote>
+      )}
+      {editSession && (
+        <CorrectedClaim handleClose={handleClose} open={editSession}></CorrectedClaim>
+      )}
+    </div>
+  );
+};
+
+export default ManageClaimsTableAction;
+
