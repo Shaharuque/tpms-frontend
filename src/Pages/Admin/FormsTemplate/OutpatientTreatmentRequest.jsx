2653a86fcc61348515c90e8a98d67c0123a617f8
yakubsiyam <itsyakubsiyam@gmail.com>
2022-10-21 11:30:20 +0600

    some changes

--- src/Pages/Admin/FormsTemplate/OutpatientTreatmentRequest.jsx
+++ src/Pages/Admin/FormsTemplate/OutpatientTreatmentRequest.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 import { AiFillCloud, AiOutlinePrinter } from "react-icons/ai";
 import { Link } from "react-router-dom";
