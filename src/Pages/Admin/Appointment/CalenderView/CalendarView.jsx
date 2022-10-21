2d21dd96c74f29478711119f1da4e8f0768afa5b
devshaik444 <shahrukhamin27@gmail.com>
2022-10-21 11:30:33 +0600

    full calender io

--- src/Pages/Admin/Appointment/CalenderView/CalendarView.jsx
+++ src/Pages/Admin/Appointment/CalenderView/CalendarView.jsx
@@ -3,34 +3,55 @@ import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import "./custom.css";
 
 import "@fullcalendar/daygrid/main.css";
 import "@fullcalendar/timegrid/main.css";
+import { useQuery } from "@tanstack/react-query";
+import Loading from "../../../../Loading/Loading";
 
 const Events = [
   {
     // this object will be "parsed" into an Event Object
-    title: "The Title1", // a property!
-    start: "2022-10-12T12:30:00",
-    end: "2022-10-12T13:30:00",
+    title: "Mrs.Shanarun", // a property!
+    // start: "2022-10-12T12:30:00",
+    // end: "2022-10-12T13:30:00",
+    date: new Date(),
+    // startTime: "10:45:00",
+    // endTime: "12:45:00",
     color: "red",
   },
   {
     // this object will be "parsed" into an Event Object
     title: "The Title2", // a property!
-    start: "2022-10-01", // a property!
-    end: "2022-10-02", // a property! ** see important note below about 'end' **
+    start: "2022-10-15T12:30:00", // a property!
+    end: "2022-10-16T14:00:00", // a property! ** see important note below about 'end' **
     color: "green",
   },
   {
     // this object will be "parsed" into an Event Object
     title: "The Title3", // a property!
-    start: "2022-10-01", // a property!
-    end: "2022-10-02", // a property! ** see important note below about 'end' **
+    start: "2022-10-15T15:30:00", // a property!
+    end: "2022-10-15T18:00:00",
     color: "purple",
   },
 ];
 const CalenderView = () => {
+  // const {
+  //   isLoading,
+  //   data: calenderEvents,
+  //   refetch,
+  // } = useQuery(["availbleEvents"], () =>
+  //   // heruko site boshbey
+  //   fetch("http://localhost:8800/api/hotels/", {
+  //     method: "GET",
+  //   }).then((res) => res.json())
+  // );
+  // console.log(calenderEvents);
+
+  // if (isLoading) {
+  //   return <Loading></Loading>;
+  // }
   return (
     <div>
       <FullCalendar
