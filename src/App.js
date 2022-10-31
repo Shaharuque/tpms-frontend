52b36a14702b9bdf2970ad131969dce741421347
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2022-10-31 17:58:25 +0600

    package issue

--- src/App.js
+++ src/App.js
@@ -152,7 +152,7 @@ import AntDate from "./Testing/AntDatePicker/AntDate";
 import ERemittance from "./Pages/Admin/Payment/ERRemitance/ERemittance";
 import FormBuilderCreate from "./Pages/Admin/Settings/Settings/FormsBuilder/FormBuilderCreate";
 import FBA from "./Pages/Admin/FormsTemplate/FBA";
-import SOAP from "./Pages/Admin/FormsTemplate/SOAP";
+// import SOAP from "./Pages/Admin/FormsTemplate/SOAP";
 import BCBATraineeUSR from "./Pages/Admin/FormsTemplate/BCBATraineeUSR";
 
 function App() {
@@ -193,7 +193,7 @@ function App() {
             path="/outpatient-treatment-request"
             element={<OutpatientTreatmentRequest></OutpatientTreatmentRequest>}
           ></Route>
-          <Route path="/soap" element={<SOAP></SOAP>}></Route>
+          {/* <Route path="/soap" element={<SOAP></SOAP>}></Route> */}
           <Route
             path="/clinic-treatment"
             element={<ClinicTreatment></ClinicTreatment>}
@@ -203,7 +203,7 @@ function App() {
             path="/BCBATraineeUSF"
             element={<BCBATraineeUSR></BCBATraineeUSR>}
           ></Route>
-          <Route path="/SOAP" element={<SOAP></SOAP>}></Route>
+          {/* <Route path="/SOAP" element={<SOAP></SOAP>}></Route> */}
           {/* ------------------------form End--------------------------- */}
           <Route
             path="/forget-password"
