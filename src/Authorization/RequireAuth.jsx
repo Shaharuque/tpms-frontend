8b84a1f43ba65d3adabd1f0f0b8e6665b99cd71e
jahids <evanjahid321@gmail.com>
2022-11-17 13:03:51 +0600

    updated all staf api implementation done

--- src/Authorization/RequireAuth.jsx
+++ src/Authorization/RequireAuth.jsx
@@ -24,7 +24,7 @@ const RequireAuth = ({ children }) => {
     // Redirect them to the /login page, but save the current location they were
     // trying to go to when they were redirected. This allows us to send them
     // along to that page after they login, which is a nicer user experience
-    // than dropping them off on the home page.
+    // than dropping them off on the home page
     return <Navigate to="/" state={{ from: location }} replace />;
   }
   return children;
