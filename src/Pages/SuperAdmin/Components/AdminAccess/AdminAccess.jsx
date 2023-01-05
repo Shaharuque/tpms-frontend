7b244bff58df9002b822c632016b9696320725e8
Shanjida Rahman Maria <94469245+Maria-07@users.noreply.github.com>
2023-01-05 11:06:58 +0600

    superADMIN SIDE BAR

--- src/Pages/SuperAdmin/Components/AdminAccess/AdminAccess.jsx
+++ src/Pages/SuperAdmin/Components/AdminAccess/AdminAccess.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const AdminAccess = () => {
-  return <div>AdminAccess.jsx</div>;
+  return (
+    <div>
+      <div id="sidebar">
+        <a href="one.php">
+          1️⃣ <span>Number one</span>
+        </a>
+        <a href="two.php">
+          2️⃣ <span>Number two</span>
+        </a>
+        <a href="three.php">
+          3️⃣ <span>Number three</span>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default AdminAccess;
