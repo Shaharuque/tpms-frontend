a35074d0f649da40d0b141689196be966e31ac8d
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2023-01-11 10:23:08 +0600

    super Admin panel added

--- src/Pages/SuperAdmin/Components/CompaniesAndFacilities/CompaniesAndFacilities.jsx
+++ src/Pages/SuperAdmin/Components/CompaniesAndFacilities/CompaniesAndFacilities.jsx
@@ -25,30 +25,26 @@ const CompaniesAndFacilities = () => {
   return (
     <div className={addFacilities ? "" : "h-[100vh]"}>
       <div className="md:flex mb-2 items-center justify-between">
-        <div className="flex items-center flex-wrap gap-2 mt-5">
-          <label className="label">
-            <span className="label-text text-[16px] font-medium text-[#9b9b9b] text-left">
-              Company
-            </span>
-          </label>
-          <form>
-            <select
-              onChange={(e) => {
-                setcomp(e.target.value);
-              }}
-              className="input-border text-gray-600 rounded-sm  text-[14px] font-medium ml-1 py-1  w-full focus:outline-none"
-            >
-              <option value="volvo">Volvo XC90</option>
-              <option value="saab">Saab 95</option>
-              <option value="mercedes adbsfs fdfgd">
-                Mercedes SLK abababab
-              </option>
-              <option value="audi">Audi TT</option>
-            </select>
-          </form>
+        <div className="">
+          <span className="label-text text-[16px] font-medium text-[#9b9b9b] text-left">
+            Company
+          </span>
+
+          <select
+            onChange={(e) => {
+              setcomp(e.target.value);
+            }}
+            className="input-border text-gray-600 rounded-sm  text-[14px] font-medium ml-1 py-1  w-full focus:outline-none"
+          >
+            <option value=""></option>
+            <option value="volvo">Volvo XC90</option>
+            <option value="saab">Saab 95</option>
+            <option value="mercedes adbsfs fdfgd">Mercedes SLK abababab</option>
+            <option value="audi">Audi TT</option>
+          </select>
         </div>
 
-        <div className=" flex items-center gap-3 flex-wrap">
+        <div className=" flex items-center gap-3 flex-wrap mt-3">
           {/* <!-- The button to open modal --> */}
 
           <button onClick={handleAddCompanyOpen} className=" pms-button ">
