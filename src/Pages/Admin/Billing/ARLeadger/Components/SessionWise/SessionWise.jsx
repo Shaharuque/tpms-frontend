0ead601189d1a729b03ee543a1925246158ec39f
devshaik444 <shahrukhamin27@gmail.com>
2023-03-15 05:33:42 -0700

    updated

--- src/Pages/Admin/Billing/ARLeadger/Components/SessionWise/SessionWise.jsx
+++ src/Pages/Admin/Billing/ARLeadger/Components/SessionWise/SessionWise.jsx
@@ -753,7 +753,129 @@ const SessionWise = () => {
                         className="input-border input-font w-full focus:outline-none"
                         {...register("aging_status")}
                       >
-                        <option value="name">EFT</option>
+                        <option value={0}></option>
+                        <option value="Corrected Claim">Corrected Claim</option>
+                        <option value="COB">COB</option>
+                        <option value="NCOF-Re-File">NCOF-Re-File</option>
+                        <option value="Appeal">Appeal</option>
+                        <option value="Reprocessing">Reprocessing</option>
+                        <option value="Medical Records">Medical Records</option>
+                        <option value="Payor Escalation">
+                          Payor Escalation
+                        </option>
+                        <option value="Provider Escalation">
+                          Provider Escalation
+                        </option>
+                        <option value="MG Escalations">MG Escalations</option>
+                        <option value="Write Off">Write Off</option>
+                        <option value="Overpayment">Overpayment</option>
+                        <option value="Timely Filing">Timely Filing</option>
+                        <option value="Paid to Patient">Paid to Patient</option>
+                        <option value="V-Mail/ Follow up">
+                          V-Mail/ Follow up
+                        </option>
+                        <option value="Paid">Paid</option>
+                        <option value="In Process">In Process</option>
+                        <option value="To call">To call</option>
+                        <option value="Authorization - Reprocess">
+                          Authorization - Reprocess
+                        </option>
+                        <option value="Maximum Benefit Reached - Reprocess">
+                          Maximum Benefit Reached - Reprocess
+                        </option>
+                        <option value="Authorization - Escalation">
+                          Authorization - Escalation
+                        </option>
+                        <option value="Maximum Benefit Reached - Escalation">
+                          Maximum Benefit Reached - Escalation
+                        </option>
+                        <option value="Need Followup">Need Followup</option>
+                        <option value="Appealed">Appealed</option>
+                        <option value="Claim sent back for reprocess">
+                          Claim sent back for reprocess
+                        </option>
+                        <option value="Claim submitted">Claim submitted</option>
+                        <option value="Sent to posting team">
+                          Sent to posting team
+                        </option>
+                        <option value="Rebill claim">Rebill claim</option>
+                        <option value="Duplicate - Reprocess">
+                          Duplicate - Reprocess
+                        </option>
+                        <option value="Coverage Terminated - Reprocess">
+                          Coverage Terminated - Reprocess
+                        </option>
+                        <option value="Coverage Terminated - Escalation">
+                          Coverage Terminated - Escalation
+                        </option>
+                        <option value="Set to pay">Set to pay</option>
+                        <option value="Lack of information">
+                          Lack of information
+                        </option>
+                        <option value="Predetermination - Reprocess">
+                          Predetermination - Reprocess
+                        </option>
+                        <option value="Predetermination - Escalation">
+                          Predetermination - Escalation
+                        </option>
+                        <option value="Non covered - reprocess">
+                          Non covered - reprocess
+                        </option>
+                        <option value="Non covered - Escalation">
+                          Non covered - Escalation
+                        </option>
+                        <option value="Requested EOB">Requested EOB</option>
+                        <option value="Awaiting for EOB">
+                          Awaiting for EOB
+                        </option>
+                        <option value="Patient Inactive - Reprocess">
+                          Patient Inactive - Reprocess
+                        </option>
+                        <option value="Patient Inactive - Escalation">
+                          Patient Inactive - Escalation
+                        </option>
+                        <option value="Copay issue">Copay issue</option>
+                        <option value="Out of Network - Reprocess">
+                          Out of Network - Reprocess
+                        </option>
+                        <option value="Out of Network - Escalation">
+                          Out of Network - Escalation
+                        </option>
+                        <option value="Under payment">Under payment</option>
+                        <option value="Additional Information - Reprocess">
+                          Additional Information - Reprocess
+                        </option>
+                        <option value="Additional information - escalation">
+                          Additional information - escalation
+                        </option>
+                        <option value="Medical records - Reprocess">
+                          Medical records - Reprocess
+                        </option>
+                        <option value="Medical records - Escalation">
+                          Medical records - Escalation
+                        </option>
+                        <option value="Invalid/Missing DX - Reprocess">
+                          Invalid/Missing DX - Reprocess
+                        </option>
+                        <option value="Invalid/Missing DX - Escalation">
+                          Invalid/Missing DX - Escalation
+                        </option>
+                        <option value="Invalid/Missing Modifier - Reprocess">
+                          Invalid/Missing Modifier - Reprocess
+                        </option>
+                        <option value="Invalid/Missing Modifier - Escalation">
+                          Invalid/Missing Modifier - Escalation
+                        </option>
+                        <option value="Need to Transmit">
+                          Need to Transmit
+                        </option>
+                        <option value="Credentialling issue - Escalation">
+                          Credentialling issue - Escalation
+                        </option>
+                        <option value="Paid not posted - Sent to posting team">
+                          Paid not posted - Sent to posting team
+                        </option>
+                        <option value="MR submitted">MR submitted</option>
                       </select>
                     </div>
                   </div>
@@ -773,7 +895,62 @@ const SessionWise = () => {
                       />
                     </div>
                   </div>
-                  {/* Zero Paid btn was here */}
+                  <div className="w-[220px]">
+                    <div>
+                      <label className="label">
+                        <span className="label-text text-[15px] font-medium text-[#9b9b9b] text-left">
+                          Selected date
+                        </span>
+                      </label>
+                      <div className="ml-1">
+                        <div className="flex  justify-between items-center text-gray-600 input-border rounded-sm px-1 mx-1 w-full">
+                          <input
+                            value={
+                              startDate
+                                ? `${startMonth} ${startDay}, ${startYear}`
+                                : `Start Date`
+                            }
+                            readOnly
+                            onClick={() => setOpenCalendar(true)}
+                            // {...register("start_date")}
+                            className="focus:outline-none font-medium text-center pb-[1.8px] text-[14px] text-gray-600 bg-transparent w-2/5 cursor-pointer"
+                          />
+                          <RiArrowLeftRightLine
+                            onClick={() => setOpenCalendar(true)}
+                            className="cursor-pointer mx-1 text-gray-600 text-[14px] font-medium w-1/5"
+                          ></RiArrowLeftRightLine>
+                          <input
+                            // defaultValue={"5-10-2034"}
+                            value={
+                              endDate
+                                ? `${endMonth} ${endDay}, ${endYear}`
+                                : `End Date`
+                            }
+                            readOnly
+                            onClick={() => setOpenCalendar(true)}
+                            // {...register("end_date")}
+                            className="focus:outline-none font-medium text-center bg-transparent text-[14px] text-gray-600 w-2/5 cursor-pointer"
+                          />
+                        </div>
+
+                        {/* Multi date picker component called */}
+                        <div
+                          ref={refClose}
+                          // className="absolute z-10 md:ml-[-9%] lg:ml-0 xl:ml-0 2xl:ml-[35%]s "
+                          className="absolute z-10 lg:ml-[0%] md:ml-[-30%] mt-1"
+                        >
+                          {openCalendar && (
+                            <CustomDateRange
+                              range={range}
+                              setRange={setRange}
+                              handleCancelDate={handleCancelDate}
+                              setOpen={setOpenCalendar}
+                            ></CustomDateRange>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </>
               )}
               <div className="flex items-center gap-2">
