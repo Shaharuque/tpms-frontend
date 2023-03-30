2dd1f5e329dc85f45ac6e316e7bc75fe8d525c7e
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2023-03-30 13:20:00 +0600

    run tested

--- src/Pages/Shared/Test.jsx
+++ src/Pages/Shared/Test.jsx
@@ -1,35 +1,7 @@
-import React, { useState } from "react";
-import { AiOutlinePlus } from "react-icons/ai";
-import CreateAppointmentAvailability from "./NavigationBar/AdditionFeatures/CreateAppointmentAvailability/CreateAppointmentAvailability";
+import React from "react";
 
 const Test = () => {
-  const [availability, setAvailability] = useState(true);
-
-  const availabilityHandler = () => {
-    setAvailability(true);
-  };
-  const availabilityHandleClose = () => {
-    setAvailability(false);
-  };
-  return (
-    <>
-      {" "}
-      <div className="ml-24 mt-24">
-        <button
-          className="text-xs text-secondary px-2 py-1 mb-2 rounded-sm hover:text-white hover:bg-secondary flex items-center font-bold gap-1 w-[110px] border border-secondary"
-          onClick={() => availabilityHandler()}
-        >
-          <AiOutlinePlus className="text-sm" /> Add Note
-        </button>
-      </div>
-      {availability && (
-        <CreateAppointmentAvailability
-          handleClose={availabilityHandleClose}
-          open={availability}
-        ></CreateAppointmentAvailability>
-      )}
-    </>
-  );
+  return <div></div>;
 };
 
 export default Test;
