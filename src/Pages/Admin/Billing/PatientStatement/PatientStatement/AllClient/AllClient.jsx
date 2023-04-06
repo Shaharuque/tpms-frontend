e4e48cc2250f24ca5bff10c6b231083967443286
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2023-04-06 11:04:18 +0600

    Patient statement added

--- src/Pages/Admin/Billing/PatientStatement/PatientStatement/AllClient/AllClient.jsx
+++ src/Pages/Admin/Billing/PatientStatement/PatientStatement/AllClient/AllClient.jsx
@@ -1,34 +1,98 @@
-import React from "react";
-import { Collapse } from "antd";
+import React, { useState } from "react";
+import { Collapse, Switch } from "antd";
+import { AiOutlineMail } from "react-icons/ai";
+import { FaRegEye } from "react-icons/fa";
+import AllClientTable from "./AllClientTable";
 const { Panel } = Collapse;
-const text = (
-  <p
-    style={{
-      paddingLeft: 24,
-    }}
-  >
-    A dog is a type of domesticated animal. Known for its loyalty and
-    faithfulness, it can be found as a welcome guest in many households across
-    the world.
-  </p>
-);
+const data = [
+  {
+    id: 1,
+    name: "Adrianne Hilley",
+  },
+  {
+    id: 2,
+    name: "Rosabella Mooring",
+  },
+  {
+    id: 3,
+    name: "Alla Grahlmans",
+  },
+  {
+    id: 4,
+    name: "Aloise Colquit",
+  },
+  {
+    id: 5,
+    name: "Cinderella Sykes",
+  },
+  {
+    id: 6,
+    name: "Judd Arghent",
+  },
+  {
+    id: 7,
+    name: "Shamus Gilby",
+  },
+  {
+    id: 8,
+    name: "Claretta Wyrall",
+  },
+  {
+    id: 9,
+    name: "Alfredo Hellsdon",
+  },
+  {
+    id: 10,
+    name: "Ainslee Jacobovitz",
+  },
+];
+
 const AllClient = () => {
+  const [submited, setSubmited] = useState(false);
   return (
-    <div>
+    <div className="my-5">
       <Collapse
+        accordion
         className=" bg-transparent"
         bordered={false}
         defaultActiveKey={["1"]}
       >
-        <Panel header={<>My name is khan</>} key="1">
-          {text}
-        </Panel>
-        <Panel header="This is panel header 2" key="2">
-          {text}
-        </Panel>
-        <Panel header="This is panel header 3" key="3">
-          {text}
-        </Panel>
+        {data.map((d, i) => (
+          <Panel
+            header={
+              <>
+                <div className="flex items-center justify-between flex-wrap">
+                  <div className="flex items-center gap-2 text-base font-medium text-gray-600">
+                    <input type="checkbox" />
+                    {d.name}
+                  </div>
+                  <div className="flex items-center gap-5">
+                    <div className="text-base">$ 45.6</div>
+                    <AiOutlineMail className=" text-base text-secondary" />
+                    <FaRegEye className=" text-base text-secondary" />
+                    <div className="flex items-center gap-2 text-sm font-regular text-gray-600">
+                      <Switch
+                        size="small"
+                        onClick={() => {
+                          setSubmited(!submited);
+                        }}
+                      />
+                      {submited ? (
+                        <span>Submitted</span>
+                      ) : (
+                        <span>Not Submitted</span>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </>
+            }
+            key={d.id}
+          >
+            <hr />
+            <AllClientTable></AllClientTable>
+          </Panel>
+        ))}
       </Collapse>
     </div>
   );
