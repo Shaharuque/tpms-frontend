09ed72eef77bbfbe450fb6c9e89a782fd4b52016
devshaik444 <shahrukhamin27@gmail.com>
2023-04-17 21:28:49 -0700

    Add services,Add CPT done

--- src/Pages/Admin/Settings/Settings/AddCptCode/AddCptCode/AddCptCodeActionModal.jsx
+++ src/Pages/Admin/Settings/Settings/AddCptCode/AddCptCode/AddCptCodeActionModal.jsx
@@ -73,7 +73,7 @@ export default function AddCptCodeActionModal({
           },
           data: {
             ...FormData,
-            cptid: cpt_id,
+            cptid: id,
           },
         });
 
