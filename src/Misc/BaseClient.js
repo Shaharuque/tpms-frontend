c31e1707917631a7002c8739bf0b37b67bb4eba9
devshaik444 <shahrukhamin27@gmail.com>
2023-05-25 01:29:49 -0700

    updated billing

--- src/Misc/BaseClient.js
+++ src/Misc/BaseClient.js
@@ -1,5 +1,5 @@
-//export const baseIp = "https://stagapi.therapypms.com/api/v1/inadmin";
-export const baseIp = "http://localhost:8080/api/v1/inadmin";
+export const baseIp = "https://stagapi.therapypms.com/api/v1/inadmin";
+//export const baseIp = "http://localhost:8080/api/v1/inadmin";
 export const headers = {
   "Content-Type": "application/json",
   Accept: "application/json",
