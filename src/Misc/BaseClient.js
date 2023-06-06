4c6b779a7e2f826039b2a1c606e7a96a20345ebf
devshaik444 <shahrukhamin27@gmail.com>
2023-06-06 19:10:37 +0600

    provider schedule

--- src/Misc/BaseClient.js
+++ src/Misc/BaseClient.js
@@ -1,7 +1,9 @@
 // export const baseIp = "https://stagapi.therapypms.com/api/v1/inadmin";
 // export const patientIp = "https://stagapi.therapypms.com/api/v1/patient";
+//export const providerIp = "https://stagapi.therapypms.com/api/v1/provider";
 export const baseIp = "http://localhost:8080/api/v1/inadmin";
 export const patientIp = "http://localhost:8080/api/v1/patient";
+export const providerIp = "http://localhost:8080/api/v1/provider";
 export const headers = {
   "Content-Type": "application/json",
   Accept: "application/json",
